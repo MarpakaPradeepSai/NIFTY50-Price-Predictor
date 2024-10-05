299491b553d8814562f49dfd2afd7ebc02cdc6aa
Marpaka Pradeep Sai <153713502+MarpakaPradeepSai@users.noreply.github.com>
2024-10-05 19:04:11 +0530

    Update nifty50_code.py

--- nifty50_code.py
+++ nifty50_code.py
@@ -47,7 +47,7 @@ def predict_next_business_days(model, data, look_back=10, days=5):
     return predictions
 
 # Streamlit app layout
-st.markdown("<h1 style='text-align: center; font-size: 50px;'>NIFTY50 Price Predictor 📈📉</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; font-size: 50px;'>NIFTY 50 Price Predictor 📈📉</h1>", unsafe_allow_html=True)
 
 # Center the NIFTY50 logo image
 st.markdown(
